cd5eed9b1471ba6a07173b3e577551fe838ac332
hamirf <haidar300400@gmail.com>
2023-08-21 21:57:07 +0700

    Popup Window Can't be Shown yet

--- MeetingRoom/Program.cs
+++ MeetingRoom/Program.cs
@@ -1,39 +1,45 @@
 using MeetingRoomWebApp.AutoGen;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddDbContext<MeetingRoomDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("MeetingRoomDatabase") ?? throw new InvalidOperationException("Connection string 'MeetingRoomDbContext' not found.")));
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddDistributedMemoryCache();
-
-builder.Services.AddSession();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public static class Program
 {
-    app.UseExceptionHandler("/User/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddDbContext<MeetingRoomDbContext>(options =>
+            options.UseSqlite(builder.Configuration.GetConnectionString("MeetingRoomDatabase") ?? throw new InvalidOperationException("Connection string 'MeetingRoomDbContext' not found.")));
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddDistributedMemoryCache();
+
+        builder.Services.AddSession();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/User/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseSession();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=User}/{action=Index}/{id?}");
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.UseSession();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=User}/{action=Index}/{id?}");
-
-app.Run();
