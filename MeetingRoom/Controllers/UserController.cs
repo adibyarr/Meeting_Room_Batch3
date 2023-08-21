c90474c93efc45f0360c665ab41dc4f9604fae5c
Bimanjaya Aji <anjay4705@gmail.com>
2023-08-21 13:34:09 +0700

    added some room data to DB. modified roomlist view and usercontroller

--- MeetingRoom/Controllers/UserController.cs
+++ MeetingRoom/Controllers/UserController.cs
@@ -7,31 +7,36 @@ namespace MeetingRoom.Controllers;
 
 public class UserController : Controller
 {
-    private readonly MeetingRoomDbContext _db;
+	private readonly MeetingRoomDbContext _db;
 
-    public UserController(MeetingRoomDbContext db)
-    {
-        _db = db;
-    }
+	public UserController(MeetingRoomDbContext db)
+	{
+		_db = db;
+	}
 
-    public IActionResult Index(int? userId)
-    {
-        userId = HttpContext.Session.GetInt32("UserID");
-        if (userId != null)
-        {
-            return View();
-        }
-        return RedirectToAction("Index", "Login");
-    }
+	public IActionResult Index(int? userId)
+	{
+		userId = HttpContext.Session.GetInt32("UserID");
+		if (userId != null)
+		{
+			return View();
+		}
+		return RedirectToAction("Index", "Login");
+	}
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+	public IActionResult Privacy()
+	{
+		return View();
+	}
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
+	
+	public IActionResult RoomList()
+	{
+		return View("RoomList");
+	}
 }
