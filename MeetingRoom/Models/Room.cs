c816db9986b1119415c1fd71404fe0454c1a63bc
Bimanjaya Aji <anjay4705@gmail.com>
2023-08-21 22:55:52 +0700

    minor change: added Required flag to roomname

--- MeetingRoom/Models/Room.cs
+++ MeetingRoom/Models/Room.cs
@@ -8,12 +8,13 @@ namespace MeetingRoomWebApp.AutoGen;
 
 public partial class Room
 {
-    [Key]
-    public long RoomId { get; set; }
+	[Key]
+	public long RoomId { get; set; }
 
-    public string RoomName { get; set; } = null!;
+	[Required]
+	public string RoomName { get; set; } = null!;
 
-    public long? Capacity { get; set; }
+	public long? Capacity { get; set; }
 
-    public string? Description { get; set; }
+	public string? Description { get; set; }
 }
