7689952181b4f7bef2ba9d134c422515260dfb67
Bimanjaya Aji <anjay4705@gmail.com>
2023-08-21 22:58:10 +0700

    constructed create room and delete room feature

--- MeetingRoom/Controllers/AdminController.cs
+++ MeetingRoom/Controllers/AdminController.cs
@@ -46,4 +46,57 @@ public class AdminController : Controller
 		List<Room> roomList = _db.Rooms.ToList();
 		return View("RoomList",roomList);
 	}
+	
+	/*
+	TODO:
+	- add warning feature if room name already exist
+	- add edit room detail feature
+	 */
+	
+	[HttpPost]
+	public IActionResult RoomList(string roomName, int capacity, string description)
+	{
+		if (ModelState.IsValid)
+		{
+			Room room = new Room
+			{
+				RoomName = roomName,
+				Capacity = capacity,
+				Description = description
+			};
+			
+			// Room? roomExisted = _db.Rooms.FirstOrDefault(r => r.RoomName == roomName);
+			var roomExisted = _db.Rooms.Where(room => room.RoomName == roomName).ToList();
+			
+			if ((roomExisted is null) || !roomExisted.Any())
+			{
+				// return RedirectToAction("RoomList");
+				_db.Rooms.Add(room);
+				_db.SaveChanges();
+			}
+
+			// _db.Rooms.Add(room);
+			// _db.SaveChanges();
+		}
+		return RedirectToAction("RoomList");
+	}
+	
+	[HttpGet]
+	public IActionResult DeleteRoom(long roomId)
+	{
+		if (ModelState.IsValid)
+		{
+			var room = _db.Rooms.Where(room => room.RoomId == roomId).ToList();
+
+			if ((room is null) || (!room.Any()))
+			{
+				return RedirectToAction("RoomList");
+			}
+
+			_db.Rooms.RemoveRange(room);
+			_db.SaveChanges();
+		}
+	
+		return RedirectToAction("RoomList");
+	}
 }
