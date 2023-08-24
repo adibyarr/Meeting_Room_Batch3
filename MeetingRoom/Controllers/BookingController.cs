be6df6267d36f440951c7eb0cd9c3cb7995e60d3
Bimanjaya Aji <anjay4705@gmail.com>
2023-08-24 22:46:10 +0700

    added AvailableRooms action. modified some action's routing address

--- MeetingRoom/Controllers/BookingController.cs
+++ MeetingRoom/Controllers/BookingController.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using MeetingRoomWebApp.AutoGen;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -25,9 +26,36 @@ public class BookingController : Controller
 		}
 	}
 	
+	[HttpPost]
+	[Route("Booking/AvailableRooms")]
+	public IActionResult AvailableRooms(long? userId, string startDate, string endDate, string startTime, string endTime, int capacity)
+	{
+		DateOnly.TryParse(startDate, out DateOnly parsedStartDate);
+		DateOnly.TryParse(endDate, out DateOnly parsedEndDate);
+		Console.WriteLine(parsedStartDate);
+		Console.WriteLine(parsedEndDate);
+		
+		TimeOnly.TryParse(startTime, out TimeOnly parsedStartTime);
+		TimeOnly.TryParse(endTime, out TimeOnly parsedEndTime);
+		Console.WriteLine(parsedStartTime);
+		Console.WriteLine(parsedEndTime);
+		
+		userId = (int?)TempData.Peek("UserID");
+		Console.WriteLine(userId);
+		
+		using (_db)
+		{
+			var user = _db.Users?.Include(u => u.Roles).FirstOrDefault(u => u.UserId == userId);
+			return View("AvailableRooms",user);
+		}
+	}
+	
+	[Route("Booking/Book")]
 	public IActionResult Book()
 	{
 		// booking logic
 		return View("Booking");
 	}
+	
+	
 }
