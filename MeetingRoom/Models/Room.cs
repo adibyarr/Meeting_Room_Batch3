5ba55d3fae9463d5744469f1aea9bad1ee16dc72
LAPTOP-J5PNV7FP\S 410 UN <anjay4705@gmail.com>
2023-08-26 14:16:00 +0700

    added link property in room model

--- MeetingRoom/Models/Room.cs
+++ MeetingRoom/Models/Room.cs
@@ -8,17 +8,19 @@ namespace MeetingRoomWebApp.AutoGen;
 
 public partial class Room
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public long RoomId { get; set; }
+	[Key]
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	public long RoomId { get; set; }
 
 	[Required]
 	public string RoomName { get; set; } = null!;
 
 	public long? Capacity { get; set; }
 
-    public string? Description { get; set; }
-    
-    [InverseProperty("Rooms")]
-    public virtual ICollection<BookedRoom> BookedRooms { get; set;} = new List<BookedRoom>();
+	public string? Description { get; set; }
+	
+	public string? Link { get; set; }
+	
+	[InverseProperty("Rooms")]
+	public virtual ICollection<BookedRoom> BookedRooms { get; set;} = new List<BookedRoom>();
 }
