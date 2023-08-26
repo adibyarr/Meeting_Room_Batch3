7ac5b56dfd52bc944939dd40e99415904be317e2
LAPTOP-J5PNV7FP\S 410 UN <anjay4705@gmail.com>
2023-08-26 14:12:55 +0700

    added FilterCapacity method to filter rooms from db based on capacity. added FilterDate method to filter filteredCap rooms according to user's date's requirements

--- MeetingRoom/Controllers/BookingController.cs
+++ MeetingRoom/Controllers/BookingController.cs
@@ -1,8 +1,12 @@
-using System.Globalization;
+// using System.Globalization;
 using MeetingRoomWebApp.AutoGen;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using CalendarAPI;
 using Microsoft.Extensions.Configuration.UserSecrets;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
 
 namespace MeetingRoom.Controllers;
 
@@ -40,8 +44,19 @@ public class BookingController : Controller
 		Console.WriteLine(parsedStartTime);
 		Console.WriteLine(parsedEndTime);
 		
+		Console.WriteLine($"parsed DateTime start : {parsedStartDate.ToDateTime(parsedStartTime)}");
+		Console.WriteLine($"parsed DateTime end : {parsedEndDate.ToDateTime(parsedEndTime)}");
+		Console.WriteLine($"DateTime now : {DateTime.Now}");
+		
+		List<Room> roomsCap = FilterCapacity(capacity);
+		foreach (var room in roomsCap)
+		{
+			Console.WriteLine($"{room.RoomName} : {room.Capacity}");
+		}
+		
+		FilterDate(roomsCap, startDate, endDate, startTime, endTime);
+		
 		userId = (int?)TempData.Peek("UserID");
-		Console.WriteLine(userId);
 		
 		using (_db)
 		{
@@ -56,6 +71,41 @@ public class BookingController : Controller
 		// booking logic
 		return View("Booking");
 	}
+
+	private List<Room> FilterCapacity(int minCap)
+	{
+		return _db.Rooms.Where(room => room.Capacity >= minCap).ToList();
+	}
 	
-	
+	private void FilterDate(
+		List<Room> rooms, 
+		string startDate, 
+		string endDate, 
+		string startTime, 
+		string endTime)
+	{
+		DateOnly.TryParse(startDate, out DateOnly parsedStartDate);
+		DateOnly.TryParse(endDate, out DateOnly parsedEndDate);
+		TimeOnly.TryParse(startTime, out TimeOnly parsedStartTime);
+		TimeOnly.TryParse(endTime, out TimeOnly parsedEndTime);
+		
+		ServiceAccountCredential credential = ServiceAccount.GenerateCredential();
+		CalendarService service = CalendarManager.GenerateService(credential);
+		
+		foreach (var room in rooms)
+		{
+			Calendar calendar = CalendarManager.GenerateCalendar(service, room.Link);
+			
+			Events events = CalendarManager.MakeRequest(
+				service,
+				calendar,
+				parsedStartDate.ToDateTime(parsedStartTime),
+				parsedEndDate.ToDateTime(parsedEndTime)
+			);
+			
+			List<Event> eventList = events.Items.ToList();
+			
+			CalendarManager.ListingEvents(eventList);
+		}
+	}
 }
