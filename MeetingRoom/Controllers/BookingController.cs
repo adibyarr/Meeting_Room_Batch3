7e811ff8d1c27ef66301fe0102b6ba4961146e09
hamirf <haidar300400@gmail.com>
2023-08-28 12:11:44 +0700

    Ongoing Logic

--- MeetingRoom/Controllers/BookingController.cs
+++ MeetingRoom/Controllers/BookingController.cs
@@ -13,7 +13,7 @@ namespace MeetingRoom.Controllers;
 public class BookingController : Controller
 {
 	private readonly MeetingRoomDbContext _db;
-	
+
 	public BookingController(MeetingRoomDbContext db)
 	{
 		_db = db;
@@ -29,7 +29,7 @@ public class BookingController : Controller
 			return View("Booking", user);
 		}
 	}
-	
+
 	[HttpPost]
 	[Route("Booking/AvailableRooms")]
 	public IActionResult AvailableRooms(long? userId, string startDate, string endDate, string startTime, string endTime, int capacity)
@@ -38,33 +38,33 @@ public class BookingController : Controller
 		DateOnly.TryParse(endDate, out DateOnly parsedEndDate);
 		Console.WriteLine(parsedStartDate);
 		Console.WriteLine(parsedEndDate);
-		
+
 		TimeOnly.TryParse(startTime, out TimeOnly parsedStartTime);
 		TimeOnly.TryParse(endTime, out TimeOnly parsedEndTime);
 		Console.WriteLine(parsedStartTime);
 		Console.WriteLine(parsedEndTime);
-		
+
 		Console.WriteLine($"parsed DateTime start : {parsedStartDate.ToDateTime(parsedStartTime)}");
 		Console.WriteLine($"parsed DateTime end : {parsedEndDate.ToDateTime(parsedEndTime)}");
 		Console.WriteLine($"DateTime now : {DateTime.Now}");
-		
+
 		List<Room> roomsCap = FilterCapacity(capacity);
 		foreach (var room in roomsCap)
 		{
 			Console.WriteLine($"{room.RoomName} : {room.Capacity}");
 		}
-		
+
 		FilterDate(roomsCap, startDate, endDate, startTime, endTime);
-		
+
 		userId = (int?)TempData.Peek("UserID");
-		
+
 		using (_db)
 		{
 			var user = _db.Users?.Include(u => u.Roles).FirstOrDefault(u => u.UserId == userId);
-			return View("AvailableRooms",user);
+			return View("AvailableRooms", user);
 		}
 	}
-	
+
 	[Route("Booking/Book")]
 	public IActionResult Book()
 	{
@@ -76,39 +76,56 @@ public class BookingController : Controller
 	{
 		return _db.Rooms.Where(room => room.Capacity >= minCap).ToList();
 	}
-	
+
+	[Obsolete]
 	private void FilterDate(
-		List<Room> rooms, 
-		string startDate, 
-		string endDate, 
-		string startTime, 
+		List<Room> rooms,
+		string startDate,
+		string endDate,
+		string startTime,
 		string endTime)
 	{
 		DateOnly.TryParse(startDate, out DateOnly parsedStartDate);
 		DateOnly.TryParse(endDate, out DateOnly parsedEndDate);
 		TimeOnly.TryParse(startTime, out TimeOnly parsedStartTime);
 		TimeOnly.TryParse(endTime, out TimeOnly parsedEndTime);
-		
+
 		// ServiceAccountCredential credential = ServiceAccount.GenerateCredential();
 		UserCredential credential = GoogleOAuth.GenerateCredential();
 		CalendarService service = CalendarManager.GenerateService(credential);
-		
+
+		DateTime start = parsedStartDate.ToDateTime(parsedStartTime);
+		DateTime end = parsedEndDate.ToDateTime(parsedEndTime);
+		// DateOnly initialDate = DateOnly.FromDateTime(DateTime.Now);
+		// TimeOnly initialTime = new TimeOnly(00, 00, 00);
+		// DateTime initial = initialDate.ToDateTime(initialTime);
+
+		TimeSpan timeBenchmark = new DateTime(end.Year, end.Month, end.Day, 24, 00, 00) - new DateTime(end.Year, end.Month, end.Day, 00, 00, 00);
+
 		foreach (var room in rooms)
 		{
 			Calendar calendar = CalendarManager.GenerateCalendar(service, room.Link);
-			
+
 			Events events = CalendarManager.MakeRequest(
 				service,
 				calendar,
-				parsedStartDate.ToDateTime(parsedStartTime),
-				parsedEndDate.ToDateTime(parsedEndTime)
+				start,
+				end
 			);
-			
+
 			// this list contains all events ranging from datetime Start - datetime End
 			// not just events that only occurs in particular time in some days.
 			// conclusion : still need some filtering process
-			List<Event> eventList = events.Items.ToList();
-			
+			List<Event> eventList = CalendarManager.GetEventList(events);
+			foreach (var anEvent in eventList)
+			{
+				start.AddMinutes(-30);
+				if (anEvent.End.DateTime == start)
+				{
+
+				}
+			}
+
 			CalendarManager.ListingEvents(eventList);
 		}
 	}
